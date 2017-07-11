d875fc9af6ae444b287cbc65e227e5218d321de2
Yibin Hu <sophie-huyibin@hotmail.com>
2017-07-11 16:37:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name = 'exasol',
       version = '6.0.1',
       description = 'EXASolution Python Package',
-      long_description = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read(),
+      long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
       author = 'EXASOL AG',
       author_email = 'support@exasol.com',
       url = 'http://www.exasol.com/',
