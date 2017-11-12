513a8985d4c0177d75595b82e3969d73b1a8c818
Andre Hacker <andrephacker@gmail.com>
2017-11-12 23:08:38 +0100

    whitespaces and CREATE SCHEMA

--- tests/data_exchange.py
+++ tests/data_exchange.py
@@ -26,22 +26,22 @@ class TestCase(unittest.TestCase):
         if 'ODBC_LOG' in os.environ:
             self.odbc_kwargs['LOGFILE'] = os.environ['ODBC_LOG']
 
-	with exasol.connect(**self.odbc_kwargs) as ecn:
-	    crs = ecn.cursor()
-    	    crs.execute('OPEN SCHEMA exasol_travis_python')
-	    crs.execute('DROP TABLE IF EXISTS data_exchange_table')
-	    crs.execute('CREATE TABLE data_exchange_table (decimal1 DECIMAL)')
-	    for i in range(0, 50):
-		rdm = random.random()
-		crs.execute('INSERT INTO data_exchange_table VALUES {}'.format(rdm))
-	    crs.commit()
+        with exasol.connect(**self.odbc_kwargs) as ecn:
+            crs = ecn.cursor()
+            crs.execute('CREATE SCHEMA IF NOT EXISTS exasol_travis_python')
+            crs.execute('DROP TABLE IF EXISTS data_exchange_table')
+            crs.execute('CREATE TABLE data_exchange_table (decimal1 DECIMAL)')
+            for i in range(0, 50):
+                rdm = random.random()
+                crs.execute('INSERT INTO data_exchange_table VALUES {}'.format(rdm))
+            crs.commit()
 
     def tearDown(self):
-	with exasol.connect(**self.odbc_kwargs) as ecn:
+        with exasol.connect(**self.odbc_kwargs) as ecn:
             crs = ecn.cursor()
             crs.execute('OPEN SCHEMA exasol_travis_python')
             crs.execute('DROP TABLE data_exchange_table')
-	    crs.commit()
+            crs.commit()
 
 
 class ODBCOnlyTest(TestCase):
@@ -82,7 +82,7 @@ class CSVTest(TestCase):
             rows = ecn.readCSV('SELECT decimal1 FROM exasol_travis_python.data_exchange_table')
             self.assertIsInstance(rows, list)
             self.assertIsInstance(rows[0], list)
-    
+
     def test_readCSV_gets_plausible_data(self):
         with exasol.connect(**self.odbc_kwargs) as ecn:
             crs = ecn.cursor()
@@ -95,8 +95,8 @@ class CSVTest(TestCase):
     def test_writeCSV_works(self):
         with exasol.connect(**self.odbc_kwargs) as ecn:
             c = ecn.cursor()
-	    c.execute('OPEN SCHEMA exasol_travis_python')
-	    c.execute('DROP TABLE IF EXISTS T')
+            c.execute('OPEN SCHEMA exasol_travis_python')
+            c.execute('DROP TABLE IF EXISTS T')
             c.execute('CREATE TABLE T (x INT, y INT)')
             ecn.writeCSV([[1,2], [3, 4]], 'T')
 
@@ -106,7 +106,7 @@ class CSVTest(TestCase):
             row1 = [x for x in rows[1]]
             expected = sorted([row0, row1])
             result = sorted([[Decimal(1), Decimal(2)], [Decimal(3), Decimal(4)]])
-	    c.execute('DROP TABLE exasol_travis_python.t')
+            c.execute('DROP TABLE exasol_travis_python.t')
             self.assertEqual(expected, result)
 
 
@@ -132,10 +132,10 @@ class PandasTest(TestCase):
     def test_writePandas_works(self):
         with exasol.connect(**self.odbc_kwargs) as ecn:
             c = ecn.cursor()
-	    c.execute('OPEN SCHEMA exasol_travis_python')
-	    c.execute('DROP TABLE IF EXISTS exasol_travis_python.t')
+            c.execute('OPEN SCHEMA exasol_travis_python')
+            c.execute('DROP TABLE IF EXISTS exasol_travis_python.t')
             c.execute('CREATE TABLE T (x INT, y VARCHAR(10))')
-            
+
             # numpy arrays are transposed:
             data = pandas.DataFrame({1: [1,2], 2: ["a","b"]})
             ecn.writePandas(data, 'T')
@@ -146,7 +146,7 @@ class PandasTest(TestCase):
             row1 = [x for x in rows[1]]
             expected = sorted([row0, row1])
             result = sorted([[Decimal(1), "a"], [Decimal(2), "b"]])
-	    c.execute('DROP TABLE exasol_travis_python.t')
+            c.execute('DROP TABLE exasol_travis_python.t')
             self.assertEqual(expected, result)
 
 
@@ -160,47 +160,48 @@ class DefaultsTest(TestCase):
         with exasol.connect(useCSV=True, **self.odbc_kwargs) as ecn:
             rows = ecn.readData('SELECT * FROM dual')
             self.assertIsInstance(rows, list, rows.__class__)
-        
+
     def test_readData_overwrite_default(self):
         with exasol.connect(**self.odbc_kwargs) as ecn:
             rows = ecn.readData('SELECT * FROM dual',
                     readCallback=exasol.csvReadCallback)
             self.assertIsInstance(rows, list, rows.__class__)
-        
+
     def test_writeData_defaults_to_pandas(self):
         with exasol.connect(**self.odbc_kwargs) as ecn:
             c = ecn.cursor()
             c.execute('OPEN SCHEMA exasol_travis_python')
-	    c.execute('DROP TABLE IF EXISTS T')
+            c.execute('DROP TABLE IF EXISTS T')
             c.execute('CREATE TABLE T (x INT, y INT)')
-    
+
             #with self.assertRaisesRegexp(AttributeError, r"object has no attribute 'to_csv'"):
             with self.assertRaises(TypeError):
                 ecn.writeData([[1,2], [3, 4]], 'T')
-	    c.execute('DROP TABLE exasol_travis_python.t')
+            c.execute('DROP TABLE exasol_travis_python.t')
 
     def test_writeData_set_default_with_connect(self):
         with exasol.connect(useCSV=True, **self.odbc_kwargs) as ecn:
             c = ecn.cursor()
             c.execute('OPEN SCHEMA exasol_travis_python')
-	    c.execute('DROP TABLE IF EXISTS T')
+            c.execute('DROP TABLE IF EXISTS T')
             c.execute('CREATE TABLE T (x INT, y INT)')
 
             ecn.writeData([[1,2], [3, 4]], 'T')
-	    c.execute('DROP TABLE T')
-        
+            c.execute('DROP TABLE T')
+
     def test_writeData_overwrite_default(self):
         with exasol.connect(**self.odbc_kwargs) as ecn:
             c = ecn.cursor()
-	    c.execute('OPEN SCHEMA exasol_travis_python')
+            c.execute('OPEN SCHEMA exasol_travis_python')
             c.execute('DROP TABLE IF EXISTS T')
             c.execute('CREATE TABLE T (x INT, y INT)')
 
             ecn.writeData([[1,2], [3, 4]], 'T',
                     writeCallback=exasol.csvWriteCallback)
-	    c.execute('DROP TABLE T')
-        
+            c.execute('DROP TABLE T')
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
 
 # vim: ts=4:sts=4:sw=4:et:fdm=indent
+
